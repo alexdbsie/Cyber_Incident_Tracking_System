be1d8fc9f45b001de119a82a8d34597391815140
alexdbsie <20093197@mydbs.ie>
2026-04-03 13:13:09 +0100

    USer registration and login working

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+def get_db():
+    return sqlite3.connect("database.db")
+
 @app.route('/')
-def home():
-    return "Cybersecurity Incident System Running "
+def login_page():
+    return render_template('login.html')
+
+@app.route('/register_page')
+def register_page():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = generate_password_hash(request.form['password'])
+
+    conn = get_db()
+    conn.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    conn.commit()
+    conn.close()
+
+    return "User registered successfully!"
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    conn = get_db()
+    user = conn.execute("SELECT password FROM users WHERE username = ?", (username,)).fetchone()
+    conn.close()
+
+    if user and check_password_hash(user[0], password):
+        return "Login successful!"
+    else:
+        return "Invalid credentials!"
 
 if __name__ == "__main__":
     app.run(debug=True)
