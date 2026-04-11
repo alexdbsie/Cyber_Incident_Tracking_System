f44fb8d7e2c029e66f3d5b98afc1adb15f3f427b
alexdbsie <20093197@mydbs.ie>
2026-04-11 13:48:41 +0100

    Rectified errors in .html files and modified them

--- setup_db.py
+++ setup_db.py
@@ -3,9 +3,9 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL,password TEXT NOT NULL)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT  NOT NULL,password TEXT NOT NULL)""")
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS incidents (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT, type TEXT,severity TEXT,description TEXT,status TEXT)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS incidents (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, severity TEXT, description TEXT, user TEXT, date TEXT)""")
 
 conn.commit()
 conn.close()
